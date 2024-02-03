3288a90a8af1601a5c33e9c05f2c6c8098ac0106
Martin Adámek <banan23@gmail.com>
2024-02-03 11:31:05 +0100

    simplify the repro to fit into a single file
    this represents the changes i always have to do before including to the codebase anyway, so better to have them here right ahead

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,34 +1,114 @@
+import * as assert from 'assert';
+
+import {
+	Collection,
+	Entity,
+	ManyToOne,
+	OneToMany,
+	OptionalProps,
+	PrimaryKey,
+	PrimaryKeyProp,
+	Property,
+	types as PropertyType,
+	Rel,
+	wrap,
+	sql,
+} from '@mikro-orm/core';
 import { MikroORM } from '@mikro-orm/sqlite';
-import { User } from './user.entity';
+
+@Entity({ abstract: true })
+abstract class TestCaseFieldsEntity {
+	@ManyToOne(() => TestCasePriorityEntity, {
+		type: PropertyType.integer,
+		eager: true
+	})
+	priority!: Rel<TestCasePriorityEntity>;
+}
+
+@Entity({
+	tableName: 'test_case',
+})
+class TestCaseEntity extends TestCaseFieldsEntity {
+	@PrimaryKey()
+	id!: number;
+
+	@OneToMany(() => 'TestCaseRevisionEntity', 'testCase', { hidden: true, orphanRemoval: true })
+	revisions = new Collection<TestCaseRevisionEntity>(this);
+
+	@Property({ version: true })
+	version!: number;
+}
+
+@Entity({
+	tableName: 'test_case_revision',
+})
+class TestCaseRevisionEntity extends TestCaseFieldsEntity {
+	@PrimaryKey()
+	id!: number;
+
+	@ManyToOne(() => TestCaseEntity, { hidden: true })
+	testCase!: Rel<TestCaseEntity>;
+
+	@Property()
+	version!: number;
+}
+
+@Entity({
+	tableName: 'test_case_priority',
+})
+class TestCasePriorityEntity {
+	@PrimaryKey()
+	id!: number;
+
+	@Property()
+	name!: string;
+}
 
 let orm: MikroORM;
 
 beforeAll(async () => {
-  orm = await MikroORM.init({
-    dbName: 'sqlite.db',
-    entities: ['dist/**/*.entity.js'],
-    entitiesTs: ['src/**/*.entity.ts'],
-    debug: ['query', 'query-params'],
-    allowGlobalContext: true, // only for testing
-  });
-  await orm.schema.refreshDatabase();
+	orm = await MikroORM.init({
+		dbName: ':memory:',
+		entities: [TestCaseEntity, TestCasePriorityEntity, TestCaseRevisionEntity],
+		debug: ['query', 'query-params'],
+		allowGlobalContext: true,
+		implicitTransactions: false
+	});
+
+	await orm.schema.refreshDatabase();
 });
 
 afterAll(async () => {
-  await orm.close(true);
+	await orm.close(true);
 });
 
-test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
-  await orm.em.flush();
-  orm.em.clear();
+it('test', async () => {
+	const testCaseRepository = orm.em.getRepository(TestCaseEntity);
+	const testCaseRevisionRepository = orm.em.getRepository(TestCaseRevisionEntity);
+	const testCasePriorityRepository = orm.em.getRepository(TestCasePriorityEntity);
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
-  await orm.em.flush();
+	await orm.em.insert(TestCasePriorityEntity, { name: 'draft' });
+	await orm.em.insert(TestCaseEntity, { priority: 1, version: 1 });
+	await orm.em.insert(TestCaseRevisionEntity, { priority: 1, testCase: 1, version: 1 })
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+	orm.em.clear();
+
+	const [[tcr]] = await Promise.all([
+		testCaseRevisionRepository.createQueryBuilder('t0')
+			.select('*')
+			.innerJoinAndSelect('t0.testCase', 't1', { version: sql`t0.version` })
+			.innerJoinAndSelect('t1.priority', 't2')
+			.getResultList(),
+		testCasePriorityRepository.findAll(),
+	]);
+
+	assert.strictEqual(tcr.priority.id, 1);
+	assert.strictEqual(tcr.priority.name, 'draft');
+
+	const actual = wrap(tcr).toJSON();
+
+	console.dir(actual, { colors: true, compact: false, depth: 2 });
+
+	assert.strictEqual(actual.priority.id, 1);
+	assert.strictEqual(actual.priority.name, 'draft');
 });
